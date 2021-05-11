34fb95f5ed4178fe9f9546ef40ce97c528e2a11d
OsmiumOP <71386574+OsmiumOP@users.noreply.github.com>
2021-05-12 00:02:51 +0530

    Add files via upload

---
+++ osm-atmrobbery/fxmanifest.lua
@@ -0,0 +1,20 @@
+-- resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'OSMIUM#0001 | https://discord.io/osmfx'
+
+description 'ADVANCED ATM ROBBERIES | COOLDOWN | FLARES | TIMERS | POLICEALERTS | THRILLS :)'
+
+client_scripts {
+    "config.lua",
+    "client/drill.lua",
+    "client/client.lua",
+}
+
+server_scripts { 
+    "config.lua",
+    "server/server.lua"
+}
+
